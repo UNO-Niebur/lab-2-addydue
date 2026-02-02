b8edbc3157e1ead1cf0ea9f9d5ed1179b076b736
addydue <10803506@nebraska.edu>
2026-02-01 20:20:45 -0600

    Magic8Ball.py changes
    Updated metadata with name, date, and assignment details. Fixed typos in the answers list and print statement.

--- Magic8Ball.py
+++ Magic8Ball.py
@@ -1,7 +1,7 @@
 #Magic8Ball.py
-#Name:
-#Date:
-#Assignment:
+#Name: Addy Duering
+#Date: 2/1/26
+#Assignment: Lab 2
 
 #We will need random for this program, import to use this package.
 import random
@@ -9,10 +9,12 @@ import random
 def main():
   #Create a list of your responses.
   print("Magic 8 Ball")
+  answers = ("Defintely NOT", "For Sure", "No clue", "Prolly not", "Fairly Likely")
   #Prompt the user for their question.
-
+input("Enter Your Question Here")
   #Answer question randomly with one of the options from your earlier list.
-
+response = random.choice(answers)
+print(responce)
 
 if __name__ == '__main__':
   main()
